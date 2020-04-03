9757bcca375e2eb9ad6d569b7e9005a5ebe17155
Gijs Lamon <Gileba@users.noreply.github.com>
2020-04-03 16:47:19 +0200

    Basic template files

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+	defined('_JEXEC') or die('Restricted access');
+
+	/** @var JDocumentHtml $this */
+	$app      	= JFactory::getApplication();
+	$doc      	= JFactory::getDocument();
+
+	/** Output as HTML5 */
+	$this->setHtml5(true);
+
+	$menu 		= $app->getMenu();
+	$params		= $app->getTemplate(true)->params;
+	$config 	= JFactory::getConfig();
+	$pageclass 	= $menu->getActive()->getParams(true)->get('pageclass_sfx');
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" 
+   xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+   <head>
+		<jdoc:include type="head" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body class="<?php echo $pageclass ? htmlspecialchars($pageclass) : 'default'; ?>">
+		<div class="offcanvas"><jdoc:include type="modules" name="offcanvas" /></div>
+		<div class="container">
+			<jdoc:include type="message" />
+			<div class="component"><jdoc:include type="component" /></div>
+		</div>
+	</body>
+</html>
