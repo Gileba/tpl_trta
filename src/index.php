3887570aabba37c0a617c15571a81af2dc6124f8
Gijs Lamon <Gileba@users.noreply.github.com>
2020-04-06 10:19:18 +0200

    Add hardcoded logo to footer (#10)

--- src/index.php
+++ src/index.php
@@ -40,7 +40,9 @@
 				<div class="component"><jdoc:include type="component" /></div>
 				<div class="bottom"><jdoc:include type="modules" name="bottom" /></div>
 			</div>
-			<div class="footer"><jdoc:include type="modules" name="footer" /></div>
+			<div class="footer">
+				<div class="logo"><img src="/templates/tpl_trta/images/logo.png" alt="<?php echo $sitename ?>" /></div>
+				<jdoc:include type="modules" name="footer" /></div>
 		</div>
 	</body>
 </html>
